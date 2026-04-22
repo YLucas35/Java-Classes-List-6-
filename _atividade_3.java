3f24b502fc61e4c762df9c6474dc492c796f9cd2
YLucas35 <0001244748@senaimgaluno.com.br>
2026-04-22 15:31:56 -0300

    Create _atividade_3.java

---
+++ _atividade_3.java
@@ -0,0 +1,24 @@
+package Java_Lista_6_Faca_Enquanto;
+
+import java.util.Scanner;
+
+public class _atividade_3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int numb, plus;
+
+        plus = 0;
+
+        System.out.println("Digite números, e eu somarei apenas aqueles que são múltiplos de 3. (até você digitar o número 0)");
+
+        do {
+            numb = sc.nextInt();
+            if (numb % 3 == 0) {
+                plus = plus + numb;
+            }
+        } while (numb != 0);
+
+        System.out.println("A soma do números múltiplos de 3 é "+plus);
+        sc.close();
+    }
+}
