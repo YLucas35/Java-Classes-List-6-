080215e807874316eae2110460df3a8efa39803e
YLucas35 <0001244748@senaimgaluno.com.br>
2026-04-22 11:37:11 -0300

    Create _atividade_1.java

---
+++ _atividade_1.java
@@ -0,0 +1,23 @@
+package Java_Lista_5_Faca_Enquanto;
+
+import java.util.Scanner;
+
+public class _atividade_1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int numb, plus, limit;
+
+        plus = 0;
+
+        System.out.println("Digite um número limite:");
+        limit = sc.nextInt();
+
+        System.out.println("Digite números até alcançar esse limite");
+        do {
+            numb = sc.nextInt();
+            plus = plus + numb;
+        } while (plus<=limit);
+        System.out.println("Você ultrapassou o limite imposto por você mesmo e a soma dos números é: "+plus);
+        sc.close();
+    }
+}
