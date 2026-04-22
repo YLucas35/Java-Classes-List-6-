984a941b093ce88803bfff0f185096e3a0a484b0
YLucas35 <0001244748@senaimgaluno.com.br>
2026-04-22 15:01:30 -0300

    Create _atividade_2.java

---
+++ _atividade_2.java
@@ -0,0 +1,22 @@
+package Java_Lista_6_Faca_Enquanto;
+
+import java.util.Scanner;
+
+public class _atividade_2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int numb, maior;
+
+        maior = 0;
+
+        System.out.println("Digite números, e eu direi quantos deles são maiores que 10. (até você digitar o número 0)");
+
+        do {
+            numb = sc.nextInt();
+            if (numb >= 10) {
+                maior = maior + 1;
+            }
+        } while (numb != 0);
+        System.out.println("Existem: "+maior+" números maiores que 10.");
+    }
+}
