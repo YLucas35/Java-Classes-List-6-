89fe2dc74434023e84f6ad76099ea031b24ac660
YLucas35 <0001244748@senaimgaluno.com.br>
2026-04-22 16:04:12 -0300

    Create _atividade_4.java

---
+++ _atividade_4.java
@@ -0,0 +1,33 @@
+package Java_Lista_6_Faca_Enquanto;
+
+import java.util.Scanner;
+
+public class _atividade_4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double numb, plus, media;
+        int count;
+
+        plus = 0;
+        count = 0;
+
+        System.out.println("Digite números, e eu farei a média apenas dos números positivos. (até você digitar o número 0)");
+
+        do {
+            numb = sc.nextInt();
+            if (numb > 0) {
+                plus = plus + numb;
+                count = count + 1;
+            }
+        } while (numb != 0);
+
+        if (count > 0) {
+            media = plus / count;
+            System.out.println("A média dos números positivos é: "+media);
+        }
+        else {
+            System.out.println("Nenhum número positivo foi digitado.");
+        }
+        sc.close();
+    }
+}
