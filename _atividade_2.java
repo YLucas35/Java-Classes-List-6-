ded3b3de295b847be110eded696cb1c26df7601d
YLucas35 <0001244748@senaimgaluno.com.br>
2026-04-22 16:24:35 -0300

    Update _atividade_2.java

--- _atividade_2.java
+++ _atividade_2.java
@@ -9,15 +9,15 @@ public class _atividade_2 {
 
         maior = 0;
 
-        System.out.println("Digite números, e eu direi quantos deles são maiores que 10. (até você digitar o número 0)");
-
         do {
+            System.out.println("Digite números, e eu direi quantos deles são maiores que 10. (até você digitar o número 0)");
             numb = sc.nextInt();
+
             if (numb >= 10) {
                 maior = maior + 1;
             }
         } while (numb != 0);
-        
+
         System.out.println("Existem: "+maior+" números maiores que 10.");
         sc.close();
     }
