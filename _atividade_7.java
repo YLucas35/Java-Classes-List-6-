4001bf33df0ff6262e27a3b5ce0e4f15dc87772c
YLucas35 <0001244748@senaimgaluno.com.br>
2026-04-26 13:44:32 -0300

    Update _atividade_7.java

--- _atividade_7.java
+++ _atividade_7.java
@@ -1,17 +1,15 @@
-package Java_Lista_6_Faca_Enquanto;
-
-import java.util.Scanner;
-
 public class _atividade_7 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int count = 1;
-        int plus = 0;
+        int count, plus;
+        
+        count = 1;
+        plus = 0;
 
         do {
             System.out.print("Digite o "+count+" número: ");
             int numb = sc.nextInt();
-            
+
             if (numb > 50) {
                 plus = plus + numb;
             }
