6efbedc0294c3fc989ea5b2e3992f99a282b0157
YLucas35 <0001244748@senaimgaluno.com.br>
2026-04-27 14:19:05 -0300

    Update _atividade_15.java

--- _atividade_15.java
+++ _atividade_15.java
@@ -6,12 +6,15 @@ public class _atividade_15 {
     public static void main(String[] args) {
         Scanner sc= new Scanner(System.in);
         int senha;
+
         System.out.println("digite sua senha");
+
         do {
             senha = sc.nextInt();
             System.out.println("digite sua senha novamente");
         }while (senha != 1234);
-        System.out.println("acesso liberado");
+
+        System.out.println("bem vindo admin!");
         sc.close();
     }
 }
