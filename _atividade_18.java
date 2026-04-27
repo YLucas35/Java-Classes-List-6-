914102d2e252210cf25961985f6e2269a362104b
YLucas35 <0001244748@senaimgaluno.com.br>
2026-04-27 15:09:08 -0300

    Update _atividade_18.java

--- _atividade_18.java
+++ _atividade_18.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class _atividade_18 {
     public static void main(String[] args) {
-        Scanner sc= new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         int numb, count, maior, menor;
 
         count = 0;
