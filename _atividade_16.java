db1bfdedeaf2dbabbc5a4d02fe679432cd3a6200
YLucas35 <0001244748@senaimgaluno.com.br>
2026-04-27 10:53:03 -0300

    Create _atividade_16.java

---
+++ _atividade_16.java
@@ -0,0 +1,24 @@
+package Java_Lista_6_Faca_Enquanto;
+
+import java.util.Scanner;
+
+public class _atividade_16 {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int numb, count;
+
+        count = 0;
+
+        System.out.println("Digite números, e eu mostrarei quantos deles estão entre 10 e 50  (até você digitar 0)");
+
+        do {
+            numb= sc.nextInt();
+            if (numb >= 10 && numb <= 50) {
+                count = count + 1;
+            }
+        } while (numb != 0);
+        System.out.println("A quantidade de números que estão entre 10 e 50 é: "+count);
+        sc.close();
+        //Leia números até digitar 0 e mostre quantos estão entre 10 e 50.
+    }
+}
