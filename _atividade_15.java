9fedfc1277f00a136c0f7f57b1c02f1d5653f071
YLucas35 <0001244748@senaimgaluno.com.br>
2026-04-27 10:33:52 -0300

    Create _atividade_15.java

---
+++ _atividade_15.java
@@ -0,0 +1,17 @@
+package Java_Lista_6_Faca_Enquanto;
+
+import java.util.Scanner;
+
+public class _atividade_15 {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int senha;
+        System.out.println("digite sua senha");
+        do {
+            senha = sc.nextInt();
+            System.out.println("digite sua senha novamente");
+        }while (senha != 1234);
+        System.out.println("acesso liberado");
+        sc.close();
+    }
+}
