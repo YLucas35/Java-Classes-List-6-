848605ffe0e088fefb6380cd3925d1c26cec3a1a
YLucas35 <0001244748@senaimgaluno.com.br>
2026-04-27 15:08:54 -0300

    Update _atividade_17.java

--- _atividade_17.java
+++ _atividade_17.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class _atividade_17 {
     public static void main(String[] args) {
-        Scanner sc= new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         int numb, count, par;
 
         count = 0;
