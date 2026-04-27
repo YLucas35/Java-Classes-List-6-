ad8a40a43a2e4c6a21f257a64aea8bbfe6990c4b
YLucas35 <0001244748@senaimgaluno.com.br>
2026-04-27 14:15:13 -0300

    Update _atividade_13.java

--- _atividade_13.java
+++ _atividade_13.java
@@ -11,15 +11,15 @@ public class _atividade_13 {
         count = 0;
         plus = 0;
       
-        System.out.println("escreva um numero qualquer e depois digite 0 para fechar");
+        System.out.println("Digite números, (digite 0 para sair do programa)");
 
         do {
             numb = sc.nextInt();
-            System.out.println("escreva mais um numero");
             count =count + 1;
             media = (numb + count) / 2.0;
             plus = plus + numb;
         } while (numb != 0);
+        
         System.out.println("Foram digitados "+count+" números!");
         System.out.println("o resultado da soma dos números é "+plus);
         System.out.println("o resultado da média dos resultados é: "+media);
