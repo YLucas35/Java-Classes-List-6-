b70da0e759359f066c0573322ffffc90dc4b7a5b
YLucas35 <0001244748@senaimgaluno.com.br>
2026-04-27 14:18:45 -0300

    Update _atividade_14.java

--- _atividade_14.java
+++ _atividade_14.java
@@ -2,19 +2,22 @@ package Java_Lista_6_Faca_Enquanto;
 
 import java.util.Scanner;
 
-public class _atividade_15 {
+public class _atividade_14 {
     public static void main(String[] args) {
-        Scanner sc= new Scanner(System.in);
-        int senha;
-        
-        System.out.println("digite sua senha");
-        
+        Scanner sc=new Scanner(System.in);
+        int nota, count;
+
+        count = 0;
+
+        System.out.println("Digite uma nota (se a nota for 0 ou maior do que 10 eu encerrei o programa)");
+
         do {
-            senha = sc.nextInt();
-            System.out.println("digite sua senha novamente");
-        }while (senha != 1234);
-        
-        System.out.println("bem vindo admin!");
+            nota = sc.nextInt();
+            count = count + 1;
+        } while (nota > 0 && nota < 10);
+
+        System.out.println("A sua nota é: "+nota);
         sc.close();
     }
 }
+
