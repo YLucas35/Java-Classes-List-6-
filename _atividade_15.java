67b7bbb18caacf3fc1205fd1f06ce1f0cfdd3270
YLucas35 <0001244748@senaimgaluno.com.br>
2026-04-27 15:08:35 -0300

    Update _atividade_15.java

--- _atividade_15.java
+++ _atividade_15.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class _atividade_15 {
     public static void main(String[] args) {
-        Scanner sc= new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         int senha;
 
         System.out.println("digite sua senha");
