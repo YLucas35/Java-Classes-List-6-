0ddadf3dc53b780f47e1d40a4ce5c0c99b9e437a
YLucas35 <0001244748@senaimgaluno.com.br>
2026-04-27 11:07:54 -0300

    Create _atividade_17.java

---
+++ _atividade_17.java
@@ -0,0 +1,26 @@
+package Java_Lista_6_Faca_Enquanto;
+
+import java.util.Scanner;
+
+public class _atividade_17 {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int numb, count, par;
+
+        count = 0;
+        par = 0;
+
+        System.out.println("Digite números, e eu somarei apenas aqueles que são pares (até você digitar 0)");
+
+        do {
+            numb = sc.nextInt();
+            if (numb % 2 == 0) {
+                par = par + numb;
+                count = count + 1;
+            }
+        } while (numb != 0);
+
+        System.out.println("A soma dos números pares é: "+par);
+        sc.close();
+    }
+}
